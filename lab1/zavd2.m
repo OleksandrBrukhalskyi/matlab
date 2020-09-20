752105847b95214b68ef9a06b6b9f9f1af0707c7
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-09-20 18:08:43 +0300

    refactor code

--- lab1/zavd2.m
+++ lab1/zavd2.m
@@ -3,5 +3,11 @@ B=[k 13 -7+k; -k 3*k 1; k+2 7 k+8];
 A=[2-k 4*k; k 6];
 det_b = det(B);
 det_a = det(A);
+disp('Матриця  A')
+disp(A)
+disp('Матриця  B')
+disp(B)
+disp('Детермінант матриці  B')
 disp(det_b);
+disp('Детермінант матриці  A')
 disp(det_a);
